9032314af39b558358a8b4f3edea3e441922135a
jaroslaw <jarek.skrzypczak@o2.pl>
2019-03-24 15:13:55 +0100

    Add external class for file picker dialog wrote by Roger Keays

--- app/src/main/java/com/jarkendar/totabs/ChooserSourceActivity.kt
+++ app/src/main/java/com/jarkendar/totabs/ChooserSourceActivity.kt
@@ -1,12 +1,23 @@
 package com.jarkendar.totabs
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_chooser_source.*
 
 class ChooserSourceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chooser_source)
+
+        choose_file_image.setOnClickListener {
+            val fileChooser = FileChooser(this@ChooserSourceActivity)
+            fileChooser.setFileListener {
+                Log.d("*****", it.absolutePath)
+                Log.d("*****", it.name)
+                Log.d("*****", it.totalSpace.toString())
+            }.showDialog()
+        }
     }
 }
