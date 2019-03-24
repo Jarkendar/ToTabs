af6d0d47e31d79e39215076dc807644cb65b4c47
jaroslaw <jarek.skrzypczak@o2.pl>
2019-03-24 13:31:23 +0100

    Add new screen

---
+++ app/src/main/java/com/jarkendar/totabs/ChooserSource.kt
@@ -0,0 +1,12 @@
+package com.jarkendar.totabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooserSource : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chooser_source)
+    }
+}
