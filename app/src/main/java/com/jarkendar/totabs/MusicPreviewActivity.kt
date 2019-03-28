779f931aa6635b0f46d7d268ece97f259df576a6
jaroslaw <jarek.skrzypczak@o2.pl>
2019-03-28 16:45:46 +0100

    Add activity to preview music

---
+++ app/src/main/java/com/jarkendar/totabs/MusicPreviewActivity.kt
@@ -0,0 +1,12 @@
+package com.jarkendar.totabs
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MusicPreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_preview)
+    }
+}
