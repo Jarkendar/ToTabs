5557bff3a38d41709e76710381b44b5361b68241
jaroslaw <jarek.skrzypczak@o2.pl>
2019-04-30 12:02:51 +0200

    Added calc array of the best frequencies

--- app/src/main/java/com/jarkendar/totabs/analyzer/Analyzer.kt
+++ app/src/main/java/com/jarkendar/totabs/analyzer/Analyzer.kt
@@ -6,6 +6,7 @@ import com.jarkendar.totabs.analyzer.wavfiles.WavFileException
 import org.jtransforms.fft.DoubleFFT_1D
 import java.io.IOException
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class Analyzer constructor(private val musicFileHolder: MusicFileHolder) {
@@ -56,9 +57,63 @@ class Analyzer constructor(private val musicFileHolder: MusicFileHolder) {
         }
         doubleFFT_1D.realForward(fftData)
         val maxFrequency = getMaxFrequency(fftData, sampleRate)
+        getMaxFrequenciesBaseOnHarmonic(fftData, sampleRate)
         Log.d("****d****", maxFrequency.toString())
     }
 
+    /**
+     * Return Array of Pair(frequency, weighted sum harmonic amplitudes)
+     */
+    private fun getMaxFrequenciesBaseOnHarmonic(fftArray: DoubleArray, sampleRate: Long): Array<Pair<Double, Double>> {
+        val amplitudes = countAmplitudes(fftArray, sampleRate)
+        val harmonicSum = countWeightedSumHarmonic(amplitudes)
+        sortArrayOfPair(harmonicSum)
+        val resultPairs = cutBestFirst(harmonicSum)
+        Log.d(TAG, Arrays.toString(resultPairs))
+        return resultPairs
+    }
+
+    private fun countAmplitudes(fftArray: DoubleArray, sampleRate: Long): Array<Pair<Double, Double>> {//Pair(frequency, amplitude)
+        val amplitudes = ArrayList<Pair<Double, Double>>(fftArray.size / 2)
+        for (i in 0 until fftArray.size / 2) {
+            val re = fftArray[2 * i]
+            val im = fftArray[2 * i + 1]
+            val freq = i * 8.0 * (sampleRate.toDouble() / fftArray.size)
+            if (freq in LOWER_BOUND_HUMAN_PERCEPT_HZ..UPPER_BOUND_HUMAN_PERCEPT_HZ) {
+                amplitudes.add(Pair(freq, Math.sqrt(re * re + im * im)))
+            }
+        }
+        return amplitudes.toTypedArray()
+    }
+
+    private fun countWeightedSumHarmonic(amplitudes: Array<Pair<Double, Double>>): Array<Pair<Double, Double>> {
+        val result = ArrayList<Pair<Double, Double>>(amplitudes.size)
+        for (i in 0 until amplitudes.size) {
+            var sum = 0.0
+            for (h in 1..HARMONIC_TO_ANALYZE) {
+                if (i * h < amplitudes.size) {
+                    sum += amplitudes[i * h].second / h
+                }
+            }
+            result.add(i, Pair(amplitudes[i].first, sum))
+        }
+        return result.toTypedArray()
+    }
+
+    /**
+     * Sorted descending by second param in pair
+     */
+    private fun sortArrayOfPair(array: Array<Pair<Double, Double>>) {
+        array.sortWith(kotlin.Comparator { o1, o2 -> -compareValues(o1.second, o2.second) })
+    }
+
+    private fun cutBestFirst(array: Array<Pair<Double, Double>>): Array<Pair<Double, Double>> {
+        val result = ArrayList<Pair<Double, Double>>(BEST_FIRST)
+        for (i in 0 until if (BEST_FIRST <= array.size) BEST_FIRST else array.size) {
+            result.add(i, array[i])
+        }
+        return result.toTypedArray()
+    }
 
     private fun getMaxFrequency(fftArray: DoubleArray, sampleRate: Long): Double {
         var maxMagnitude = 1.0
@@ -78,5 +133,12 @@ class Analyzer constructor(private val musicFileHolder: MusicFileHolder) {
 
     private fun printArray(doubleArray: DoubleArray) {
         Log.d("****doubleArray****", Arrays.toString(doubleArray))
+    }
+
+    companion object {
+        private val HARMONIC_TO_ANALYZE = 4
+        private val BEST_FIRST = 6
+        private val UPPER_BOUND_HUMAN_PERCEPT_HZ = 20_000.0
+        private val LOWER_BOUND_HUMAN_PERCEPT_HZ = 20.0
     }
 }
