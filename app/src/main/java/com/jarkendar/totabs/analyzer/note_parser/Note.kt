8cc6b7de9821e062206180cc56967f731295e8ad
jaroslaw <jarek.skrzypczak@o2.pl>
2019-05-02 13:54:58 +0200

    Added amplitude as inner field in note

--- app/src/main/java/com/jarkendar/totabs/analyzer/note_parser/Note.kt
+++ app/src/main/java/com/jarkendar/totabs/analyzer/note_parser/Note.kt
@@ -2,7 +2,9 @@ package com.jarkendar.totabs.analyzer.note_parser
 
 class Note constructor(val name: String, val frequency: Double) {
 
+    var amplitude: Double = 0.0
+
     override fun toString(): String {
-        return "Note(name='$name', frequency=$frequency)"
+        return "Note(name='$name', frequency=$frequency, amplitude=$amplitude)"
     }
 }
