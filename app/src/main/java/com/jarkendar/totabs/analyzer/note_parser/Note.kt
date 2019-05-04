f8c57243340cc040586bafa1e22cb7e6f230db30
jaroslaw <jarek.skrzypczak@o2.pl>
2019-05-04 14:57:08 +0200

    Added length param to Note

--- app/src/main/java/com/jarkendar/totabs/analyzer/note_parser/Note.kt
+++ app/src/main/java/com/jarkendar/totabs/analyzer/note_parser/Note.kt
@@ -3,9 +3,10 @@ package com.jarkendar.totabs.analyzer.note_parser
 class Note constructor(val name: String, val frequency: Double) {
 
     var amplitude: Double = 0.0
+    var length = NoteLength.FULL
 
     override fun toString(): String {
-        return "Note(name='$name', frequency=$frequency, amplitude=$amplitude)"
+        return "Note(name='$name', frequency=$frequency, amplitude=$amplitude, length=${length.name})"
     }
 
     public fun copy(): Note {
