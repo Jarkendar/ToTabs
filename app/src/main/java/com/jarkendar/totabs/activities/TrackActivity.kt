064fba5cf2cd2a65c0e6b4d4e9a7e6e95099be0c
jaroslaw <jarek.skrzypczak@o2.pl>
2019-05-07 10:34:33 +0200

    Added set height of images views on track activity

--- app/src/main/java/com/jarkendar/totabs/activities/TrackActivity.kt
+++ app/src/main/java/com/jarkendar/totabs/activities/TrackActivity.kt
@@ -1,10 +1,13 @@
 package com.jarkendar.totabs.activities
 
+import android.graphics.Point
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.jarkendar.totabs.R
 import com.jarkendar.totabs.analyzer.Track
+import kotlinx.android.synthetic.main.activity_track.*
+
 
 class TrackActivity : AppCompatActivity() {
 
@@ -17,5 +20,16 @@ class TrackActivity : AppCompatActivity() {
 
         track = intent.extras.getSerializable(MusicPreviewActivity.TRACK_EXTRA_NAME) as Track
         Log.d(TAG, track.toString())
+
+        setHeightOfImagesViews()
+    }
+
+    private fun setHeightOfImagesViews() {
+        val size = Point()
+        windowManager.defaultDisplay.getSize(size)
+        val screenHeight = size.y
+        val height = ((screenHeight - ((3 + 4) * resources.getDimension(R.dimen.margin_8dp) + resources.getDimension(R.dimen.button_height))) / 2.2).toInt()
+        staff_imageView.layoutParams.height = height
+        tablature_imageView.layoutParams.height = height
     }
 }
