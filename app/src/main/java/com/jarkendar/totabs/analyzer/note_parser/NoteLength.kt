845871622beed7814edeff99230f08fabc23402c
jaroslaw <jarek.skrzypczak@o2.pl>
2019-05-08 09:54:28 +0200

    Added params describing the appearance of notes

--- app/src/main/java/com/jarkendar/totabs/analyzer/note_parser/NoteLength.kt
+++ app/src/main/java/com/jarkendar/totabs/analyzer/note_parser/NoteLength.kt
@@ -1,10 +1,10 @@
 package com.jarkendar.totabs.analyzer.note_parser
 
-enum class NoteLength(val length: Double) {
-    FULL(4.0),
-    HALF(2.0),
-    QUARTER(1.0),//is base
-    EIGHTH(0.5),
-    SIXTEENTH(0.25),
-    THIRTY_SECOND(0.125)
+enum class NoteLength(val length: Double, val fill: Boolean, val column: Boolean, val numberOfTails: Int) {
+    FULL(4.0, false, false, 0),
+    HALF(2.0, true, false, 0),
+    QUARTER(1.0, true, true, 0),//is base
+    EIGHTH(0.5, true, true, 1),
+    SIXTEENTH(0.25, true, true, 2),
+    THIRTY_SECOND(0.125, true, true, 3)
 }
