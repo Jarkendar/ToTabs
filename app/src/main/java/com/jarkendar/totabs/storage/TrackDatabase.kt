a0bfe1543f796bfb24a215a6179098443b8e13dc
jaroslaw <jarek.skrzypczak@o2.pl>
2019-06-12 18:47:24 +0200

    Added static fields describe fields in database

--- app/src/main/java/com/jarkendar/totabs/storage/TrackDatabase.kt
+++ app/src/main/java/com/jarkendar/totabs/storage/TrackDatabase.kt
@@ -24,11 +24,32 @@ public class TrackDatabase constructor(val context: Context) : SQLiteOpenHelper(
 
     private fun upgradeDatabase(sqLiteDatabase: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         //todo create database
+
+
     }
 
 
     companion object {
         private val TAG = "********"
+
+        private val FIELD_ROW_ID: String = "_id"
+
+        private val TABLE_TRACK: String = "TABLE_TRACK"
+
+        private val FIELD_TRACK_NAME: String = "TRACK_NAME"
+        private val FIELD_BEATS_PER_MINUTE: String = "BEATS_PER_MINUTE"
+        private val FIELD_MIN_NOTE: String = "MINIMAL_NOTE"
+        private val FIELD_MIN_NOTE_DURATION: String = "MINIMAL_NOTE_DURATION"
+        private val FIELD_LIST_OF_NOTES: String = "LIST_OF_NOTES"
+
+        private val TABLE_NOTES: String = "TABLE_NOTES"
+
+        private val FIELD_NOTE_NAME: String = "NOTE_NAME"
+        private val FIELD_FREQUENCY: String = "FREQUENCY"
+        private val FIELD_STAFF_POSITION: String = "STAFF_POSITION"
+        private val FIELD_IS_HALF_TONE: String = "IS_HALF_TONE"
+        private val FIELD_AMPLITUDE: String = "AMPLITUDE"
+        private val FIELD_LENGTH: String = "LENGTH"
     }
 
 }
